d7383d86136f8133b54ce18c3eb6c2d0b3cbb17c
deeplearn009 <sragimov@vk.com>
2025-04-09 18:10:32 +0400

    first commit

---
+++ eBay/src/App.jsx
@@ -0,0 +1,45 @@
+import React, {useState} from 'react'
+import Header from './components/header/Header'
+import Products from './components/products/Products'
+import Cards from './components/cards/Card'
+import './App.scss'
+import axios from 'axios'
+
+function App() {
+  const [data, setData] = useState([]);
+  window.onload = () => {
+    axios.get("https://fakestoreapi.com/products").then((res) => {
+      setData(res.data);
+    });
+  };
+
+  return (
+    <>
+    
+    <Header/>
+    <Products/>
+
+    <div className='cards'>
+
+      <div className="content">
+
+        <h1 className='title'>Best Selling</h1>
+
+        <div className='pr' style={{display: "flex", justifyContent: "center", alignContent: "center", flexWrap: "wrap", width: "100%", gap: "40px"}}>
+          {data && data.map(item => <Cards item={item}/>)}
+        </div>
+
+
+
+      </div>
+
+     
+
+    </div>
+    </>
+  );
+
+
+}
+
+export default App
---
+++ eBay/src/components/products/Products.jsx
@@ -0,0 +1,14 @@
+import React, { useState } from "react";
+import styles from "./Products.module.scss";
+
+const Products = () => {
+  return (
+    <div className={styles.container}>
+      <div className={styles.title}>
+        <h1>Cameras & Photo</h1>
+      </div>
+    </div>
+  );
+};
+
+export default Products;
